731bdc9699e2a9b638ab44a16ba1734eb5abe9bd
leka642 <lekasreesree@gmail.com>
2025-05-10 21:06:37 +0530

    Fix: Display project images in ProjectContainer

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -1,6 +1,4 @@
 import uniqid from 'uniqid';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LaunchIcon from '@mui/icons-material/Launch';
 import './ProjectContainer.css';
 
 const ProjectContainer = ({ project }) => (
@@ -28,25 +26,7 @@ const ProjectContainer = ({ project }) => (
       </ul>
     )}
 
-    {project.sourceCode && (
-      <a
-        href={project.sourceCode}
-        aria-label='source code'
-        className='link link--icon'
-      >
-        <GitHubIcon />
-      </a>
-    )}
-
-    {project.livePreview && (
-      <a
-        href={project.livePreview}
-        aria-label='live preview'
-        className='link link--icon'
-      >
-        <LaunchIcon />
-      </a>
-    )}
+  
   </div>
 );
 
